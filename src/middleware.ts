3234eddab8f030b66fadb3af753688f048b46dcc
ratul544388 <ratulhossainbnc@gmail.com>
2024-03-03 15:35:45 +0600

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/anyone-can-visit-this-route'],
-  ignoredRoutes: ['/no-auth-in-this-route'],
+  publicRoutes: [
+    "/api/webhooks(.*)",
+    "/",
+  ],
 });
  
 export const config = {
